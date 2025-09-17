ee7c7b64b709a6397b4dcb23466ec8a006643f97
karthi582002 <karthi582002@gmail.com>
2025-09-17 23:36:45 +0530

    added cors for deployed frontend

--- src/app.js
+++ src/app.js
@@ -2,12 +2,22 @@ import express from 'express';
 import nurseRoutes from "./routes/nurseRoutes.js";
 import cors from "cors";
 const app = express();
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://crud-frontend-black-delta.vercel.app/"
+];
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: true,
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true,
 }));
 
